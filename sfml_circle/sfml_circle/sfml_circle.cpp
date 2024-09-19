1e74e55ef4ef5899c49030a3a311a9a45acc222a
JD-edu <96219601+JD-edu@users.noreply.github.com>
2024-09-19 19:49:01 +0900

    Sep 19th  1

--- sfml_circle/sfml_circle/sfml_circle.cpp
+++ sfml_circle/sfml_circle/sfml_circle.cpp
@@ -1,57 +1,24 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 
-class Enemy {
-public:
-    sf::CircleShape c;
-    float radius = 20.f;
-    Enemy() {
-        c.setPosition(rand() % 640, 0);
-        c.setFillColor(sf::Color::Yellow);
-        c.setRadius(radius);
-    }
-    // 오버로드 사용예 
-    Enemy(float x, float y) {
-        c.setPosition(x, y);
-        c.setFillColor(sf::Color::Green);
-        c.setRadius(radius);
-    }
-
-    void update(sf::RenderWindow& win) {
-        c.move(0.f, 1.f);
-        win.draw(c);
-    }
-private:
-
-};
-
-class ZigZag : public Enemy {
-
-public:
-    // 오버라이드의 사용예 
-    void update(sf::RenderWindow &win) {
-        zigzag *= -1;
-        c.move(zigzag, 1.f);
-        win.draw(c);
-    }
-private:
-    int zigzag = 20;
-};
-
-
+// 네임스페이스의 만드는 예
+namespace uga {
+    int dol = 0;
+}
 
 int main()
 {
-    Enemy e1;
-    Enemy e2(0, 240);
-    ZigZag z1;
-  
+    // uga 네임스페이스에 속한 변수 사용법 
+    std::cout << uga::dol << std::endl;
+    //sf::VideoMode vd(640, 480);
+    //sf::RenderWindow window(vd, "Ugauga doldol");
     sf::RenderWindow window(sf::VideoMode(640, 480), "My SFML Window");
-  
-    sf::CircleShape blue(30.f);
-    blue.setFillColor(sf::Color::Blue);
+    // 빨간 원 생성 (반지름 20) ============
+    sf::CircleShape circle1(20.f);
+    circle1.setFillColor(sf::Color::Red);
 
-    blue.setPosition(320, 400);
+    // 원의 위치 설정 (창의 중앙)
+    circle1.setPosition(200, 200);
 
     window.setFramerateLimit(60);
     while (window.isOpen())
@@ -59,23 +26,14 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
-            else if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::A)
-
-                    blue.move(-5.f, 0.f); // 왼쪽으로 이동
-                else if (event.key.code == sf::Keyboard::D)
-                    blue.move(5.f, 0.f); // 오른쪽으로 이동
             }
         }
-     
+
+        circle1.setPosition(200, 200);
         window.clear(sf::Color::Black);
-        e1.update(window);
-        e2.update(window);
-        z1.update(window);
-        window.draw(blue);
+        window.draw(circle1); // 원 그리기
         window.display();
     }
     return 0;
