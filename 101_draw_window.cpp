b8f967c8f3c739586a42bab99d702c3e3a40d761
JD-edu <96219601+JD-edu@users.noreply.github.com>
2024-09-19 19:52:05 +0900

    Sep 19th -2

---
+++ 101_draw_window.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), "My SFML Window");
+   
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+ 
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
+}
